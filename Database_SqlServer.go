c71c86a6ded26f7a16e67055b2fb4c1c5b19496e
alexhendra <alexhendra_panjaitan@yahoo.com>
2016-09-08 14:42:33 +0700

    Coba kembali connect ke SQL Server tapi belum bisa juga

--- Database_SqlServer.go
+++ Database_SqlServer.go
@@ -1,1 +1,72 @@
-package belajargolang
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+type tBaseStatus struct {
+	StatusId   string
+	StatusName string
+	Descript   string
+	Categ      string
+	Aktif      bool
+}
+
+func connect() *sql.DB {
+	dsn := "server=172.16.64.109;user id=845879;password=5879;database=JobSchDB"
+	var db, err = sql.Open("mssql", dsn)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	return db
+}
+
+func sqlQuery() {
+	var db = connect()
+
+	defer db.Close()
+
+	var rows, err = db.Query("select StsId,StsName,Descript,Category,Active from tBase_Status")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	defer rows.Close()
+
+	var result []tBaseStatus
+
+	for rows.Next() {
+		var each = tBaseStatus{}
+		var err = rows.Scan(&each.StatusId, &each.StatusName,
+			&each.Descript, &each.Categ, &each.Aktif)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(0)
+		}
+
+		result = append(result, each)
+	}
+
+	if err = rows.Err(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	for _, each := range result {
+		fmt.Println(each.StatusName)
+	}
+}
+
+func main() {
+	//var db = connect()
+	//defer db.Close()
+
+	sqlQuery()
+}
