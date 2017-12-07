9ad7d674f76a92c3a60e55c5c190ef3f9a630726
Alex Hendra <alexhendra_panjaitan@yahoo.com>
2017-12-07 17:17:27 +0700

    perbaikan content

--- array.go
+++ array.go
@@ -4,19 +4,21 @@ import "fmt"
 
 func main() {
 	// array adalah kumpulan data bertipe sama
-	/*var data [4]int
-	 for i := 0; i < len(data); i++ {
-	 	data[i] = i
-	 }
 
-	 for i := 0; i < len(data); i++ {
-	 	fmt.Println("Data [", i, "] =", i)
-	 }*/
+	// *** Cara 1 ***
+	var data [4]int
+	for i := 0; i < len(data); i++ {
+		data[i] = i
+	}
 
-	// cara horizontal
-	/* var fruits = [4]string{"apple", "grape", "banana", "melon"}
+	for i := 0; i < len(data); i++ {
+		fmt.Println("Data [", i, "] =", data[i])
+	}
+
+	// *** Cara 2 (horizontal) ***
+	var fruits = [4]string{"apple", "grape", "banana", "melon"}
 	 fmt.Println("Jumlah	element	\t\t", len(fruits))
-	 fmt.Println("Isi semua element \t", fruits)*/
+	 fmt.Println("Isi semua element \t", fruits)
 
 	// cara	vertikal
 	/* var fruits = [4]string{
@@ -29,15 +31,28 @@ func main() {
 	// fmt.Println("Jumlah	element	\t\t", len(fruits))
 	// fmt.Println("Isi semua element \t", fruits)
 
+	// *** Cara 3 ***
 	// Deklarasi array boleh tidak dituliskan jumlah item array-nya
 	// Jumlah item akan dikalkulasi otomatis menyesuaikan data elemen yang diisikan
-	/*var data = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println("isi dari array :", data)*/
+	var bilangan = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("isi dari array :", bilangan)
 
-	// Membuat dan menentukkan jumlah elemen array dengan "make"
+	// *** Cara 4 ***
+	// Membuat dan menentukkan jumlah elemen array dengan statement "make"
 	// make([]tipedatanya,jumlahelemennya)
 	var arrayku = make([]string,2)
 	arrayku[0] = "alex"
 	arrayku[1] = "hendra"
 	fmt.Println(arrayku)
+
+
+	// *** Array 2 dimensi ***
+	arr2:=[2][3] int {}
+	for i:=0; i<2; i++ {
+		for j:=0; j<3; j++ {
+			arr2[i][j]=i+j
+		}
+	}
+
+	fmt.Println("2d: ", arr2)
 }
